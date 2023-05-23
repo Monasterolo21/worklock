726eb723aaf5ee44344f4610ca38af74aa13dae8
Samuele Monasterolo <smonasterolo2@gmail.com>
2023-05-23 20:27:54 +0200

    first version

---
+++ src/components/TimerComponent.vue
@@ -0,0 +1,214 @@
+<template>
+  <div>
+    <div class="round-corner">
+      <div class="timer">
+        {{ timer }}
+      </div>
+    </div>
+
+    <div class="set-section">
+      <h2>SET DONE</h2>
+      <div class="set-container">
+        <button class="action-button" @click="decrementCounter">
+          <span class="material-symbols-rounded"> remove </span>
+        </button>
+        <span class="counter">{{ counter }}</span>
+        <button class="action-button" @click="incrementCounter">
+          <span class="material-symbols-rounded"> add </span>
+        </button>
+      </div>
+    </div>
+
+    <div class="action-section">
+      <button @click="startTimer" :disabled="disableTimerButton">
+        <span class="material-symbols-rounded"> play_pause </span>
+      </button>
+      <button @click="reset" :disabled="disableResetButton">
+        <span class="material-symbols-rounded"> device_reset </span>
+      </button>
+      <button @click="cheer">
+        <span class="material-symbols-rounded"> fitness_center </span>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 0,
+      disableResetButton: true,
+      disableTimerButton: true,
+      timer: 0,
+      timerActive: false,
+      intervalId: null,
+    };
+  },
+  methods: {
+    incrementCounter() {
+      this.counter++;
+      this.disableResetButton = false;
+      this.disableTimerButton = false;
+      this.reset();
+      this.toggleTimer();
+    },
+    decrementCounter() {
+      if (this.counter > 0) {
+        this.counter--;
+      }
+      if (this.counter === 0) {
+        this.disableResetButton = true;
+        this.disableTimerButton = true;
+      }
+      this.reset();
+      this.toggleTimer();
+    },
+    reset() {
+      if (this.timerActive) {
+        this.timerActive = false;
+        clearInterval(this.intervalId);
+        this.timer = 0;
+      }
+    },
+    cheer() {
+      alert("Dai uomo!");
+    },
+
+    toggleTimer() {
+      if (this.timerActive) {
+        this.stopTimer();
+      } else {
+        this.startTimer();
+      }
+    },
+    startTimer() {
+      if (!this.timerActive) {
+        this.timerActive = true;
+        this.intervalId = setInterval(() => {
+          this.timer++;
+        }, 1000);
+      }
+    },
+
+    stopTimer() {
+      if (this.timerActive) {
+        this.timerActive = false;
+        clearInterval(this.intervalId);
+      }
+    },
+  },
+};
+</script>
+
+<style>
+.round-corner {
+  border-radius: 100%;
+  background-color: transparent;
+  color: var(--secondary-color);
+  padding: 1em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  align-items: center;
+  border: 2px solid var(--secondary-color);
+  /* make same height that width */
+  width: 10em;
+  height: 10em;
+}
+
+.set-section {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 4em 1em 1em;
+  /* align content vertically */
+  flex-direction: column;
+  padding: 0;
+}
+
+.set-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0em 0;
+  flex-direction: row;
+  padding: 0;
+}
+
+.set-section h2 {
+  margin: 0;
+  font-size: 1.6em;
+  color: var(--primary-color);
+  display: block;
+  width: 100%;
+  padding: 0;
+  padding-bottom: -2em;
+  font-weight: 500;
+}
+
+.timer {
+  font-size: 3em;
+  color: var(--text-color);
+}
+
+.counter {
+  display: flex;
+  align-items: center;
+  font-size: 3em;
+  vertical-align: middle;
+  padding-bottom: 0.2em;
+  color: var(--text-color);
+}
+
+.action-button {
+  background-color: transparent;
+  border: 1px solid var(--primary-color);
+  border-radius: 50%;
+  width: auto;
+  height: auto;
+  margin: 0 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  color: var(--primary-color);
+  padding: 0.15em;
+  font-size: 2em;
+  vertical-align: top;
+}
+
+button:disabled {
+  opacity: 0.5;
+  cursor: not-allowed;
+}
+
+button {
+  background-color: transparent;
+  border: 1px solid var(--primary-color);
+  border-radius: 100%;
+  width: auto;
+  height: auto;
+  margin: 0 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  color: var(--primary-color);
+  padding: 0.2em;
+  font-size: 1.6em;
+  margin: 0.3em;
+  vertical-align: top;
+}
+
+.action-section {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 1em 1em;
+  /* align content vertically */
+  flex-direction: row;
+  padding: 0;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <template>
-  <h1>Home</h1>
+  <div class="content-middle">
+    <timer-component></timer-component>
+  </div>
 </template>
 
 <script>
+import TimerComponent from "@/components/TimerComponent.vue";
 export default {
+  components: { TimerComponent },
   name: "HomeView",
 };
 </script>
